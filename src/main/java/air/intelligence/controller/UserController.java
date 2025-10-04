a5d5162b790395e4906dfc74fc5012d638703434
PGD <rudeh1253@gmail.com>
2025-10-04 14:22:04 +0900

    feat: define coordinate value

--- src/main/java/air/intelligence/controller/UserController.java
+++ src/main/java/air/intelligence/controller/UserController.java
@@ -1,7 +1,7 @@
 package air.intelligence.controller;
 
 import air.intelligence.constant.UserConstant;
-import air.intelligence.domain.Coord;
+import air.intelligence.value.Coord;
 import air.intelligence.service.UserService;
 import air.intelligence.util.api.BaseResponse;
 import air.intelligence.util.http.CookieUtil;
@@ -9,11 +9,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.Duration;
 
 @RestController
 @RequestMapping("/api/v1/users")
