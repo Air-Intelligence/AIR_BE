62f1f2ae746edb1386eee4d419a15d9e6fddfb64
PGD <rudeh1253@gmail.com>
2025-10-05 04:59:11 +0900

    refactor: Mock Weather data sort

--- src/main/java/air/intelligence/value/GeoFeature.java
+++ src/main/java/air/intelligence/value/GeoFeature.java
@@ -1,10 +1,8 @@
 package air.intelligence.value;
 
-import java.util.Map;
-
 public record GeoFeature (
         String type,
         Geometry geometry,
-        Map<String, Object> properties
+        GeoProperties properties
 ) {
 }
