53108b533cb56ef582c337287f46f925b7a96515
PGD <rudeh1253@gmail.com>
2025-10-04 10:08:47 +0900

    chore: implement ErrorResponse

--- src/main/java/air/intelligence/util/api/ErrorResponse.java
+++ src/main/java/air/intelligence/util/api/ErrorResponse.java
@@ -5,9 +5,21 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
+
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @ToString
 public class ErrorResponse {
+    private final String errorName;
+    private final String message;
+    private LocalDateTime timestamp;
 
+    public static ErrorResponse from(ErrorSpec errorSpec) {
+        return new ErrorResponse(
+                errorSpec.name(),
+                errorSpec.getMessage(),
+                LocalDateTime.now()
+        );
+    }
 }
